3507140a3abfa05cba676ac0e14a88a0ad6be1e0
Robin <s1186143@student.windesheim.nl>
2023-06-05 17:41:01 +0200

    Currently broken. trying to load items based upon Location

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using MVCLibraryApp.Models;
 using System.Diagnostics;
+using System.Linq;
 
 namespace MVCLibraryApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context; // Define the context variable here
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context) // Inject the context in the constructor
         {
             _logger = logger;
+            _context = context; // Assign the injected context to the private variable
         }
 
         public IActionResult Index()
@@ -27,6 +32,24 @@ namespace MVCLibraryApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Dashboard()
+        {
+            // Get the list of locations to populate the dropdown
+            ViewBag.Locations = _context.Locaties.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Dashboard(int locationId)
+        {
+            // Get the list of locations to populate the dropdown
+            ViewBag.Locations = _context.Locaties.ToList();
+
+            // Get the list of items for the selected location
+            ViewBag.Items = _context.Items.Where(i => i.LocatieID == locationId).ToList();
+
+            return View();
         }
     }
 }
---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCLibraryApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCLibraryApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ItemsController
+        public async Task<ActionResult> Index(string searchString)
+        {
+            var items = from i in _context.Items
+                        select i;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                items = items.Where(i => i.Titel.Contains(searchString)
+                                       || i.Auteur.Contains(searchString)
+                                       || i.Locatie.Beschrijving.Contains(searchString));
+            }
+
+            return View(await items.ToListAsync());
+        }
+    }
+}
