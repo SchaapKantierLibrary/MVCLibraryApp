a0cb64176e6c5a1618774db0b41ca0d84f807f49
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2023-06-07 14:49:11 +0200

    Function having an error hopefully working asap

--- Controllers/BeheerderController.cs
+++ Controllers/BeheerderController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MVCLibraryApp.Models;
 
 [Authorize(Roles = "Beheerder")]
 public class BeheerderController : Controller
 {
+
+    private List<BezoekerModel> bezoekers = new List<BezoekerModel>();
     public IActionResult Dashboard()
     {
+        ViewBag.Bezoekers = bezoekers;
         return View();
     }
 }
