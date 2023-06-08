d96b074dcb46f72e5f4e561f49c6414b61b6cdfd
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2023-06-08 10:27:40 +0200

    Connection with roles done succesfully

--- Controllers/BeheerderController.cs
+++ Controllers/BeheerderController.cs
@@ -7,15 +7,27 @@ using MVCLibraryApp.Models;
 [Authorize(Roles = "Beheerder")]
 public class BeheerderController : Controller
 {
-    private UserManager<BezoekerModel> userManager;
+    private readonly UserManager<BezoekerModel> _userManager;
+
     public BeheerderController(UserManager<BezoekerModel> userManager)
     {
-        this.userManager = userManager;
+        _userManager = userManager;
     }
 
     public async Task<IActionResult> Dashboard()
     {
-        ViewBag.Bezoekers = await userManager.Users.ToListAsync();
-        return View();
+        var users = await _userManager.Users.ToListAsync();
+        var usersWithRoles = new List<(BezoekerModel, IList<string>)>();
+
+        foreach (var user in users)
+        {
+            var roles = await _userManager.GetRolesAsync(user);
+            usersWithRoles.Add((user, roles));
+        }
+
+        ViewBag.Bezoekers = users;
+        ViewBag.Rol = usersWithRoles.ToDictionary(x => x.Item1.Id, x => x.Item2);
+
+        return View(users);
     }
 }
