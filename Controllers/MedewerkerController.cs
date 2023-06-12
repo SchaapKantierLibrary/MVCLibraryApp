1908fd6200162887ba76d7a868f2481ff28fad9c
Robin <s1186143@student.windesheim.nl>
2023-06-12 17:26:04 +0200

    Added Abonnementen beheer

--- Controllers/MedewerkerController.cs
+++ Controllers/MedewerkerController.cs
@@ -729,6 +729,132 @@ namespace MVCLibraryApp.Controllers
             return RedirectToAction(nameof(Dashboard));
         }
 
+        // GET: AbonnementModels
+        public async Task<IActionResult> IndexAbonnement()
+        {
+            return _context.Abonnementen != null ?
+                        View(await _context.Abonnementen.ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Abonnementen'  is null.");
+        }
 
+      
+    
+
+        // GET: AbonnementModels/Create
+        public IActionResult CreateAbonnement()
+        {
+            return View();
+        }
+
+        // POST: AbonnementModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAbonnement([Bind("ID,Type,MaximaleItems,Uitleentermijn,Verlengingen,Reserveringskosten,Boetekosten,Abonnementskosten")] AbonnementModel abonnementModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(abonnementModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(abonnementModel);
+        }
+
+        // GET: AbonnementModels/Edit/5
+        public async Task<IActionResult> EditAbonnement(int? id)
+        {
+            if (id == null || _context.Abonnementen == null)
+            {
+                return NotFound();
+            }
+
+            var abonnementModel = await _context.Abonnementen.FindAsync(id);
+            if (abonnementModel == null)
+            {
+                return NotFound();
+            }
+            return View(abonnementModel);
+        }
+
+        // POST: AbonnementModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditAbonnement(int id, [Bind("ID,Type,MaximaleItems,Uitleentermijn,Verlengingen,Reserveringskosten,Boetekosten,Abonnementskosten")] AbonnementModel abonnementModel)
+        {
+            if (id != abonnementModel.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(abonnementModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AbonnementModelExists(abonnementModel.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(abonnementModel);
+        }
+
+        // GET: AbonnementModels/Delete/5
+        public async Task<IActionResult> DeleteAbonnement(int? id)
+        {
+            if (id == null || _context.Abonnementen == null)
+            {
+                return NotFound();
+            }
+
+            var abonnementModel = await _context.Abonnementen
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (abonnementModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(abonnementModel);
+        }
+
+        // POST: AbonnementModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Abonnementen == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Abonnementen'  is null.");
+            }
+            var abonnementModel = await _context.Abonnementen.FindAsync(id);
+            if (abonnementModel != null)
+            {
+                _context.Abonnementen.Remove(abonnementModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AbonnementModelExists(int id)
+        {
+            return (_context.Abonnementen?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
     }
+
 }
+
